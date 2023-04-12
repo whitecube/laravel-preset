dfbbc65b3bbf15f5b758d3eb0749f603fd577790
Adrien Leloup <adrien@whitecube.be>
2023-04-12 18:39:39 +0200

    Fixed order of execution

--- src/Preset.php
+++ src/Preset.php
@@ -18,8 +18,8 @@ class Preset extends LaravelPreset
         Sass::install($command);
         Assets::install($command);
         Storage::install($command);
-        Pest::install($command);
         Composer::install($command);
+        Pest::install($command);
 
         $command->info('Installing NPM packages...');
         shell_exec('yarn');
