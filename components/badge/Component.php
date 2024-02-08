d293211b0cbce866d759e008c3ac18d275e0d425
LisaBomb <lisa@whitecube.be>
2024-02-08 09:54:54 +0100

    Added component stubs

---
+++ components/badge/Component.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Illuminate\View\ComponentAttributeBag;
+use Whitecube\BemComponents\HasBemClasses;
+
+class Badge extends Component
+{
+    use HasBemClasses;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+    ) {
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.badge');
+    }
+}
