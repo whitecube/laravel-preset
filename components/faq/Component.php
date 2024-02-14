33adac8561b676b8c1db7ac8dbae4222508b2045
LisaBomb <lisa@whitecube.be>
2024-02-14 14:16:43 +0100

    Fixed faq file

--- components/faq/Component.php
+++ components/faq/Component.php
@@ -1,45 +1,21 @@
 <?php
 
-namespace Whitecube\LaravelPreset\Components\Publishers;
+namespace App\View\Components;
 
-use Whitecube\LaravelPreset\Components\File;
-use Whitecube\LaravelPreset\Components\FilesCollection;
-use Whitecube\LaravelPreset\Components\PublisherInterface;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Whitecube\BemComponents\HasBemClasses;
 
-class Faq implements PublisherInterface
+class Faq extends Component
 {
-    /**
-     * Get the component's displayable name.
-     */
-    public function label(): string
-    {
-        return 'Faq';
-    }
+    use HasBemClasses;
 
     /**
-     * Let the publisher prompt for eventual extra input
-     * and return a collection of publishable files.
+     * Get the view / contents that represent the component.
      */
-    public function handle(): FilesCollection
+    public function render(): View|Closure|string
     {
-        $style = File::makeFromStub(
-            stub: 'components/faq/part.scss',
-            destination: resource_path('sass/parts/_faq.scss'),
-        );
-
-        $view = File::makeFromStub(
-            stub: 'components/faq/view.blade.php',
-            destination: resource_path('views/components/faq.blade.php'),
-        );
-
-        return FilesCollection::make([$style, $view]);
-    }
-
-    /**
-     * Get the component's usage instructions
-     */
-    public function instructions(): ?string
-    {
-        return "1. Add `@import 'parts/faq';` to `resources/sass/app.scss`\r\n2. Use the blade component: `<x-faq><p>Some content</p></x-faq>`";
+        return view('components.faq');
     }
 }
