c33d16d0c424f96b0327edf92641987ebdce8d3e
LisaBomb <lisa@whitecube.be>
2024-02-15 10:21:51 +0100

    Added pagination

---
+++ components/pagination/Component.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Illuminate\View\ComponentAttributeBag;
+use Whitecube\BemComponents\HasBemClasses;
+
+class Pagination extends Component
+{
+    use HasBemClasses;
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.pagination');
+    }
+}
