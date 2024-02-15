951a346bf76c9f6edbea07b43d014161accbef92
LisaBomb <lisa@whitecube.be>
2024-02-15 13:51:02 +0100

    Added loader

---
+++ components/loader/Component.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Illuminate\View\ComponentAttributeBag;
+use Whitecube\BemComponents\HasBemClasses;
+
+class Loader extends Component
+{
+    use HasBemClasses;
+}
---
+++ src/Components/Publishers/Loader.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Whitecube\LaravelPreset\Components\Publishers;
+
+use Whitecube\LaravelPreset\Components\File;
+use Whitecube\LaravelPreset\Components\FilesCollection;
+use Whitecube\LaravelPreset\Components\PublisherInterface;
+
+class Loader implements PublisherInterface
+{
+    /**
+     * Get the component's displayable name.
+     */
+    public function label(): string
+    {
+        return 'Loader';
+    }
+
+    /**
+     * Let the publisher prompt for eventual extra input
+     * and return a collection of publishable files.
+     */
+    public function handle(): FilesCollection
+    {
+        $style = File::makeFromStub(
+            stub: 'components/loader/style.scss',
+            destination: resource_path('sass/parts/_loader.scss'),
+        );
+
+        $view = File::makeFromStub(
+            stub: 'components/loader/view.blade.php',
+            destination: resource_path('views/components/loader.blade.php'),
+        );
+
+        $component = File::makeFromStub(
+            stub: 'components/Loader/Component.php',
+            destination: base_path('app/View/Components/Loader.php'),
+        );
+
+        return FilesCollection::make([
+            $style,
+            $view,
+            $component,
+        ]);
+    }
+
+    /**
+     * Get the component's usage instructions
+     */
+    public function instructions(): ?string
+    {
+        return "1. Add `@import 'parts/loader';` to `resources/sass/app.scss`\r\n2. Use the blade component: `<x-loader tag=\"a\" href=\"/example\" modifier=\"small\" icon=\"arrow-right\">Content</x-loader>`.\r\nBy default the component's tag is a \"loader\". If it's an `a`, it should also have an `href` attribute.\r\n3. You should add the possible icons to the \$availabl-icons variable at the beggining of the 'parts/_loader.scss' file.";
+    }
+}
