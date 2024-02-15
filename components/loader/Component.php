58ccd4cbdde74e976faece1cad287fe172e776a0
LisaBomb <lisa@whitecube.be>
2024-02-15 14:00:31 +0100

    update loader file

--- components/loader/Component.php
+++ components/loader/Component.php
@@ -11,4 +11,12 @@ use Whitecube\BemComponents\HasBemClasses;
 class Loader extends Component
 {
     use HasBemClasses;
+
+     /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.loader');
+    }
 }
