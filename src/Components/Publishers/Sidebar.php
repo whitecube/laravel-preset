9b784a9e8f59f0009ae8475d15f07b911d319dd7
théoo <90846584+theokbokki@users.noreply.github.com>
2024-03-25 16:01:53 +0100

    Add sidebar component

---
+++ src/Components/Publishers/Sidebar.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Whitecube\LaravelPreset\Components\Publishers;
+
+use Whitecube\LaravelPreset\Components\File;
+use Whitecube\LaravelPreset\Components\FilesCollection;
+use Whitecube\LaravelPreset\Components\PublisherInterface;
+
+class Sidebar implements PublisherInterface
+{
+    /**
+     * Get the component's displayable name.
+     */
+    public function label(): string
+    {
+        return 'Sidebar';
+    }
+
+    /**
+     * Let the publisher prompt for eventual extra input
+     * and return a collection of publishable files.
+     */
+    public function handle(): FilesCollection
+    {
+        $style = File::makeFromStub(
+            stub: 'components/sidebar/style.scss',
+            destination: resource_path('sass/parts/_sidebar.scss'),
+        );
+
+        $view = File::makeFromStub(
+            stub: 'components/sidebar/view.blade.php',
+            destination: resource_path('views/components/sidebar.blade.php'),
+        );
+
+        $component = File::makeFromStub(
+            stub: 'components/sidebar/Component.php',
+            destination: base_path('app/View/Components/Sidebar.php'),
+        );
+
+        return FilesCollection::make([
+            $style,
+            $view,
+            $component,
+        ]);
+    }
+
+    /**
+     * Get the component's usage instructions
+     */
+    public function instructions(): ?string
+    {
+        return "1. Add `@import 'parts/sidebar';` to `resources/sass/app.scss`\r\n2. Use the blade component: `<x-sidebar/>`";
+    }
+}
