967d28ea2e10509b5fab040764fea5084d73be6e
théoo <90846584+theokbokki@users.noreply.github.com>
2024-04-09 16:14:35 +0200

    Add Post and Container components

--- src/Components/Publishers/Post.php
+++ src/Components/Publishers/Post.php
@@ -23,7 +23,7 @@ class Post implements PublisherInterface
     public function handle(): FilesCollection
     {
         $style = File::makeFromStub(
-            stub: 'components/post/part.scss',
+            stub: 'components/post/style.scss',
             destination: resource_path('sass/parts/_post.scss'),
         );
 
@@ -32,7 +32,12 @@ class Post implements PublisherInterface
             destination: resource_path('views/components/post.blade.php'),
         );
 
-        return FilesCollection::make([$style, $view]);
+        $component = File::makeFromStub(
+            stub: 'components/post/Component.php',
+            destination: base_path('app/View/Components/Post.php'),
+        );
+
+        return FilesCollection::make([$style, $view, $component]);
     }
 
     /**
@@ -40,6 +45,6 @@ class Post implements PublisherInterface
      */
     public function instructions(): ?string
     {
-        return "1. Add `@import 'parts/post';` to `resources/sass/app.scss`\r\n2. Use the blade component: `<x-post><p>Some content</p></x-post>`";
+        return "1. Add `@import 'parts/post';` to `resources/sass/app.scss`\r\n2. Use the blade component: `<x-post title=\"Thirty Years Later, a Speed Boost for Quantum Factoring\" text=\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus velit suscipit, sodales risus id, malesuada est. Fusce vestibulum felis diam, nec fringilla lorem molestie ac.\" infos=\"Posté le 06/01/2023 par Laura Dattaro\" link=\"#\" more=\"Read article\" />`";
     }
 }
